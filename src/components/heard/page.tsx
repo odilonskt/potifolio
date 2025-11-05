1ef08e2616c1a1b72e93835dcb2910896db6077a
odilonskt <odilon123c@gmail.com>
2025-11-05 11:12:55 -0300

    feat: add heard

---
+++ src/components/heard/page.tsx
@@ -0,0 +1,19 @@
+export default function Heard() {
+    return (
+        <>
+        <header className="bg-white text-black rounded-full   cursor-pointer  flex justify-center
+mx-auto   mt-2  absolute inset-0 m-auto w-200 h-10 "> 
+            
+            <nav className="columns-10 p-4 shadow-md flex justify-between items-center  w-full " >
+                <h5>odilon</h5>
+            <div>
+            <ul className=" flex gap-4">
+               <li> <a href="#">inicio</a></li>
+              <li><a href="">meio </a></li>
+                <li> <a href="">fim</a></li>
+            </ul>
+            </div>
+            </nav>
+            </header></>    
+    );
+}
