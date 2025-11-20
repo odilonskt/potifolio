301963de7ad5500f94932b860300b96c8babd3b6
odilonskt <odilon123c@gmail.com>
2025-11-20 08:47:32 -0300

    fix: add bg black

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Heard from "@/components/heard/page";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,11 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
-         <Heard/>
+        <Heard />
         {children}
       </body>
     </html>
