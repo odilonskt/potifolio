822133ae3f98947e5908dd4a79ef2324e3e91e3f
odilonskt <odilon123c@gmail.com>
2025-11-24 13:58:24 -0300

    fix: remove function handleDownLoadCV for  NextLink

--- src/components/start/page.tsx
+++ src/components/start/page.tsx
@@ -7,12 +7,6 @@ interface StartProps {
   id: string;
 }
 
-const handleDownloadCV = () => {
-  const link = document.createElement("a");
-  link.href = "/Odilon_CV.pdf";
-  link.download = "Odilon_CV.pdf";
-  link.click();
-};
 export default function Start({ id }: StartProps) {
   return (
     <main id={id} className="text-white w-full">
@@ -35,7 +29,10 @@ export default function Start({ id }: StartProps) {
             Criando soluções digitais inovadoras com tecnologias modernas
           </p>
           <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-6 mt-2 sm:mt-4 ">
-            <NextLink href="https://linkedin.com/odilon-dev/" target="_blank">
+            <NextLink
+              href="https://www.linkedin.com/in/odilon-dev/"
+              target="_blank"
+            >
               <Linkedin
                 size={32}
                 color="white"
@@ -50,15 +47,15 @@ export default function Start({ id }: StartProps) {
                 className="hover:scale-110 transition-transform duration-200 hover:opacity-80"
               />
             </NextLink>
-
-            <Button
-              variant="outline"
-              className="flex gap-2 items-center text-black bg-white hover:bg-gray-100 px-4 py-2 sm:px-6 sm:py-3 min-w-[140px] sm:min-w-[160px] text-sm sm:text-base font-medium transition-all duration-200 hover:scale-105"
-              onClick={handleDownloadCV}
-            >
-              <Download size={20} color="black" />
-              Meu CV
-            </Button>
+            <NextLink href="/Odilon_CV.pdf" download>
+              <Button
+                variant="outline"
+                className="flex gap-2 items-center text-black bg-white hover:bg-gray-100 px-4 py-2 sm:px-6 sm:py-3 min-w-[140px] sm:min-w-[160px] text-sm sm:text-base font-medium transition-all duration-200 hover:scale-105"
+              >
+                <Download size={20} color="black" />
+                Meu CV
+              </Button>
+            </NextLink>
           </div>
         </div>
       </div>
