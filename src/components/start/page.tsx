5857bf43a0ef36abbf0ad4d6061d92815d556282
odilonskt <odilon123c@gmail.com>
2025-11-24 12:23:13 -0300

    feat: add function HandleDownloadCV

--- src/components/start/page.tsx
+++ src/components/start/page.tsx
@@ -1,21 +1,29 @@
+"use client";
 import { Download, Github, Linkedin } from "@deemlol/next-icons";
 import Image from "next/image";
 import NextLink from "next/link";
 import { Button } from "../ui/button";
+interface StartProps {
+  id: string;
+}
 
-export default function Start() {
+const handleDownloadCV = () => {
+  const link = document.createElement("a");
+  link.href = "/Odilon_CV.pdf";
+  link.download = "Odilon_CV.pdf";
+  link.click();
+};
+export default function Start({ id }: StartProps) {
   return (
-    <main
-      className=" text-white   w-full
-    "
-    >
+    <main id={id} className="text-white w-full">
       <div className="flex flex-col lg:flex-row items-center justify-center min-h-screen px-4 sm:px-6 lg:px-8 py-8 lg:py-0 gap-8 lg:gap-12">
         <div className="flex items-center flex-shrink-0">
           <Image
-            src="/perfil.png"
-            alt="Perfil"
-            width={250}
-            height={250}
+            src="/perfil.svg"
+            alt="Foto de perfil de Odilon - Desenvolvedor Full-Stack"
+            width={350}
+            height={350}
+            priority={true}
             className="rounded-xl w-32 h-32 sm:w-40 sm:h-40 md:w-56 md:h-56 lg:w-64 lg:h-64 xl:w-72 xl:h-72 object-cover shadow-lg"
           />
         </div>
@@ -46,6 +54,7 @@ export default function Start() {
             <Button
               variant="outline"
               className="flex gap-2 items-center text-black bg-white hover:bg-gray-100 px-4 py-2 sm:px-6 sm:py-3 min-w-[140px] sm:min-w-[160px] text-sm sm:text-base font-medium transition-all duration-200 hover:scale-105"
+              onClick={handleDownloadCV}
             >
               <Download size={20} color="black" />
               Meu CV
