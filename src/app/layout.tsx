e2ba353cdb37b8f47ed2649081077ca5333689ed
odilonskt <odilon123c@gmail.com>
2025-11-24 12:16:58 -0300

    remove: heard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-import Heard from "@/components/heard/page";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import "./globals.css";
 import Footer from "@/components/footer/page";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
