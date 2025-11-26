3c55faa91d29410935a899f826a6cf598340fb94
odilonskt <odilon123c@gmail.com>
2025-11-26 16:30:49 -0300

    fix: trocando os  label  & aumentado a % de w

--- src/components/heard/page.tsx
+++ src/components/heard/page.tsx
@@ -2,13 +2,16 @@
 
 export default function Header() {
   const menuItems = [
-    { label: "Início", href: "#start" },
-    { label: "Meio", href: "#meio" },
-    { label: "Fim", href: "#fim" },
+    { label: "Introdução", href: "#start" },
+    { label: "sobre", href: "#meio" },
+    // { label: "Projeto", href: "#Projeto" },
+    { label: "Tecnologia", href: "#Tecnologia" },
+    // { label: "localização", href: "#location" },
+    // { label: "Contato", href: "#Contato" },
   ];
 
   return (
-    <header className="bg-white text-black rounded-full fixed top-2 sm:top-4 left-1/2 transform -translate-x-1/2 z-50 w-[95%] sm:w-[90%] md:w-[85%] max-w-4xl mx-auto shadow-lg border border-gray-100">
+    <header className="bg-white text-black rounded-full fixed top-2 sm:top-4 left-1/2 transform -translate-x-1/2 z-50 w-[98%] sm:w-[90%] md:w-[85%] max-w-4xl mx-auto shadow-lg border border-gray-100">
       <nav className="px-3 sm:px-6 md:px-8 py-2 sm:py-3 flex justify-between items-center gap-2 sm:gap-4">
         {/* Logo */}
         <h5 className="text-lg sm:text-xl md:text-2xl font-bold whitespace-nowrap flex-shrink-0 tracking-wide">
