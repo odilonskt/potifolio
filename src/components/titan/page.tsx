b06bcc5af62062a126c72f77a9f5c8b5f00eecd8
odilonskt <odilon123c@gmail.com>
2025-11-26 16:26:04 -0300

    feat: add props id

--- src/components/titan/page.tsx
+++ src/components/titan/page.tsx
@@ -1,11 +1,15 @@
 interface TitanProps {
   title: string;
+  id?: string;
 }
 
 export default function Titan(props: TitanProps) {
   return (
     <>
-      <h1 className="text-white text-center font-bold   m-6 text-4xl md:text-5xl lg:text-6xl">
+      <h1
+        className="text-white text-center font-bold   m-6 text-4xl md:text-5xl lg:text-6xl"
+        id={props.id}
+      >
         {props.title}
       </h1>
     </>
