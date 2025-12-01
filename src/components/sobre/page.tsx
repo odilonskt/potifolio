3ae4f2f99d4e336c4b195e1737b91ab3031e8254
odilonskt <odilon123c@gmail.com>
2025-12-01 18:39:37 -0300

    feat:  colcoando alguns palavras em negirtos

--- src/components/sobre/page.tsx
+++ src/components/sobre/page.tsx
@@ -7,7 +7,64 @@ interface SobreProps {
   id: string;
 }
 
+function formatarTextoComNegrito(texto: string, palavrasNegrito: string[]) {
+  if (!palavrasNegrito.length) return texto;
+
+  const regex = new RegExp(`(${palavrasNegrito.join("|")})`, "gi");
+  const partes = texto.split(regex);
+
+  return partes.map((parte, index) =>
+    palavrasNegrito.some(
+      (palavra) => parte.toLowerCase() === palavra.toLowerCase()
+    ) ? (
+      <strong key={index} className="font-bold text-white ">
+        {parte}
+      </strong>
+    ) : (
+      <span key={index}>{parte}</span>
+    )
+  );
+}
+
 export default function Sobre(props: SobreProps) {
+  const palavrasHardSkills = [
+    "Full Stack",
+    "JavaScript",
+    "TypeScript",
+    "Node.js",
+    "Express",
+    "React",
+    "APIs RESTful",
+    "Single-Page Applications",
+    "SPA",
+    "MySQL",
+    "Git",
+    "GitHub",
+    "JavaScript/TypeScript",
+    "Nest.js",
+    "Next.js",
+    "GitHub",
+    "Git/GitHub ",
+    "Hub",
+    "PostgreSQL",
+  ];
+
+  const palavrasSoftSkills = [
+    "curioso",
+    "proativo",
+    "comunicativo",
+    "aprendizado",
+    "adaptação",
+    "trabalho em equipe",
+  ];
+
+  const palavrasEstudo = [
+    "Análise e Desenvolvimento de Sistemas",
+    "PUC Minas",
+    "Desenvolvedor Web",
+    "Programadores do Amanhã",
+    "Desenvolvedor Full Stack",
+  ];
   return (
     <div
       className="w-full flex flex-col items-center px-4 sm:px-6 lg:px-8 py-8 sm:py-12"
@@ -24,7 +81,10 @@ export default function Sobre(props: SobreProps) {
                        leading-relaxed sm:leading-loose md:leading-loose
                        text-justify sm:text-left"
           >
-            {props.descrition_hardSkill}
+            {formatarTextoComNegrito(
+              props.descrition_hardSkill,
+              palavrasHardSkills
+            )}
           </p>
           <br></br>
           <p
@@ -32,7 +92,10 @@ export default function Sobre(props: SobreProps) {
                        leading-relaxed sm:leading-loose md:leading-loose
                        text-justify sm:text-left"
           >
-            {props.descrition_softSkill}
+            {formatarTextoComNegrito(
+              props.descrition_softSkill,
+              palavrasSoftSkills
+            )}
           </p>
           <br></br>
           <p
@@ -40,9 +103,10 @@ export default function Sobre(props: SobreProps) {
                        leading-relaxed sm:leading-loose md:leading-loose
                        text-justify sm:text-left"
           >
-            {props.descrition_estudo}
+            {formatarTextoComNegrito(props.descrition_estudo, palavrasEstudo)}
           </p>
         </div>
+        <br></br>
       </div>
     </div>
   );
